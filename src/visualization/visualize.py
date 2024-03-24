701b6b1e6ba7ae187dfacf5064d43ab4f6e25fdc
michaelajao <ajaoolarinoyemichael@gmail.com>
2024-03-24 04:47:20 +0000

    Update visualization settings

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,28 +5,66 @@ import seaborn as sns
 
 # plt.style.use('seaborn-v0_8-whitegrid')
 # plt.style.available
-plt.rcParams.update({
-    "font.family": "serif",
-    "figure.facecolor": "white",
-    "axes.facecolor": "white",
-    "axes.titlesize": 26,
-    "axes.labelsize": 12,
-    "figure.figsize": [15, 8],
-    "figure.autolayout": True,
-    "legend.fontsize": "medium",
-    "legend.frameon": False,
-    "legend.loc": "best",
-    "lines.linewidth": 2.5,
-    "lines.markersize": 10,
-    "font.size": 14,
-    "xtick.labelsize": 12,
-    "ytick.labelsize": 12,
-    "figure.dpi": 300,
-    "savefig.dpi": 300,
-    "savefig.format": "pdf",
-    "savefig.bbox": "tight",
+# plt.rcParams.update({
+#     "font.family": "serif",
+#     "figure.facecolor": "white",
+#     "axes.facecolor": "white",
+#     "axes.titlesize": 26,
+#     "axes.labelsize": 12,
+#     "figure.figsize": [15, 8],
+#     "figure.autolayout": True,
+#     "legend.fontsize": "medium",
+#     "legend.frameon": False,
+#     "legend.loc": "best",
+#     "lines.linewidth": 2.5,
+#     "lines.markersize": 10,
+#     "font.size": 14,
+#     "xtick.labelsize": 12,
+#     "ytick.labelsize": 12,
+#     "figure.dpi": 300,
+#     "savefig.dpi": 300,
+#     "savefig.format": "pdf",
+#     "savefig.bbox": "tight",
     
+# })
+
+plt.rcParams.update({
+    # Font settings for clarity and compatibility with academic publications
+    "font.family": "serif",  # Consistent font family
+    # "font.serif": ["Times", "Computer Modern Roman"],  # Preferred serif fonts
+    "font.size": 14,  # Base font size for better readability
+    "text.usetex": False,  # Enable LaTeX for text rendering for a professional look
+
+    # Figure aesthetics & size for detailed visuals and fit on publication pages
+    "figure.figsize": (15, 8),  # Adjusted figure size for a balance between detail and fit
+    "figure.facecolor": "white",  # White figure background for clean print
+    "figure.autolayout": True,  # Enable automatic layout adjustments
+    "figure.dpi": 400,  # High resolution figures
+    "savefig.dpi": 400,  # High resolution saving settings
+    "savefig.format": "pdf",  # Save figures in PDF format for publications
+    "savefig.bbox": "tight",  # Tight bounding box around figures
+
+    # Axes aesthetics for clarity and precision
+    "axes.labelsize": 14,  # Clear labeling with larger font size
+    "axes.titlesize": 20,  # Prominent titles for immediate recognition
+    "axes.facecolor": "white",  # White axes background
+
+    # Legend aesthetics for distinguishing plot elements
+    "legend.fontsize": 12,  # Readable legend font size
+    "legend.frameon": False,  # No frame around legend for cleaner look
+    "legend.loc": "best",  # Optimal legend positioning
+
+    # Line aesthetics for clear visual distinctions
+    "lines.linewidth": 2,  # Thicker lines for visibility
+    "lines.markersize": 8,  # Slightly smaller markers for balance
+
+    # Tick label sizes for readability
+    "xtick.labelsize": 12, 
+    "ytick.labelsize": 12,
+    "xtick.direction": "in",  # Ticks inside the plot
+    "ytick.direction": "in",  # Ticks inside the plot
 })
+
 
 plt.rcParams.keys()
 first_data = "../../data/interim/pickle/nhs_region_data.pkl"
@@ -125,3 +163,7 @@ plt.yticks(np.arange(0, len(pivot_new_admissions_data.index), step=max(1, len(pi
 plt.tight_layout()
 plt.savefig(f"../../reports/figures/new_admissions_heatmap.png")
 plt.show()
+
+
+# plot england data with the lockdown periods
+england_data = 
