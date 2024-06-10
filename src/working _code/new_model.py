e07755cceffc375f47a62b2364dd00e3fc1a7194
Michael Ajao-olarinoye <ajaoolarinoyemichael@gmail.com>
2024-06-10 22:48:19 +0100

    refactor: Update number of layers in EpiNet and ParameterNet models

--- src/working _code/new_model.py
+++ src/working _code/new_model.py
@@ -521,7 +521,7 @@ scaled_data, scaler = scale_data(data, features, device)
 
 # Initialize model, optimizer, and scheduler
 model = EpiNet(num_layers=5, hidden_neurons= 20, output_size=8).to(device)
-parameter_net = ParameterNet(num_layers=4, hidden_neurons=20).to(device)
+parameter_net = ParameterNet(num_layers=5, hidden_neurons=20).to(device)
 optimizer = optim.Adam(list(model.parameters()) + list(parameter_net.parameters()), lr=1e-4)
 scheduler = StepLR(optimizer, step_size=5000, gamma=0.9)
 
@@ -665,4 +665,3 @@ def plot_outputs(model, parameter_net, data, device, scaler):
 
 # Plot the outputs and parameters
 plot_outputs(model, parameter_net, data, device, scaler)
-
