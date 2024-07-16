8706110611436c7cd58e5fbe994899c6b4597888
michaelajao <ajaoolarinoyemichael@gmail.com>
2024-07-16 23:21:48 +0000

    refactor: Update England metrics in CSV file

--- src_code/working _code/3experiment.py
+++ src_code/working _code/3experiment.py
@@ -496,7 +496,7 @@ def train_model(
 
 # Initialize model, optimizer, and scheduler
 model = EpiNet(num_layers=5, hidden_neurons=20, output_size=8).to(device)
-parameter_net = ParameterNet(num_layers=4, hidden_neurons=20, output_size=6).to(device)
+parameter_net = ParameterNet(num_layers=5, hidden_neurons=20, output_size=6).to(device)
 optimizer = optim.AdamW(
     list(model.parameters()) + list(parameter_net.parameters()), lr=1e-4, weight_decay=1e-2
 )
